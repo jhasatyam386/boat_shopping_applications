5c8d809d77dd3fb8ee6b18e0d384c0ef8c9e83d4
satyam jha <jhasatyam386@gamil.com>
2023-05-17 12:10:06 +0530

    17 may 2023

---
+++ boat_shopping_applications/TableViewCellLast.swift
@@ -0,0 +1,22 @@
+//
+//  TableViewCellLast.swift
+//  boat_shopping_applications
+//
+//  Created by R&W on 21/05/35.
+//
+
+import UIKit
+
+class TableViewCellLast: UITableViewCell {
+
+    @IBOutlet weak var labelForMore: UILabel!
+    override func awakeFromNib() {
+        super.awakeFromNib()
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+    }
+
+}
--- boat_shopping_applications/ViewControllerMoreLast.swift
+++ boat_shopping_applications/ViewControllerMoreLast.swift
@@ -7,23 +7,36 @@
 
 import UIKit
 
-class ViewControllerMoreLast: UIViewController {
-
+class ViewControllerMoreLast: UIViewController,UITableViewDelegate,UITableViewDataSource{
+ 
+    @IBOutlet weak var tableviewForMore: UITableView!
+    var arrForMore = ["Rate Us","Share The App","","boAt Plugin Labz","boAt Warranty Policy","boAty Werables App","boAt X Bira","boAt x Masaba","boAt | DC","boAtXMarvel","Brand To Trust","Bulk Orders","Career Opportunities","Composition of Board Committees","Dance Thought Life","Do What #FloatsYourboAt","E-Waste Managments Policy","Earn Rewards Referring Friends","Five Days Of Fashion","Flash Smart Watch Launch","Frequently Asked Questions","Gifting","Investor Relations","Policies","Privacy Policy","PROITUP","Queen of Hip Hop","Refferal","Return Policy","Security","Service Center List","Social Responsibility","Sunburn","Sunburn","Tearm of Service","Terms of Use","The boAt ProGear app","The boAthead Vibe","Track Your Order","TRebel","Warranty & Support","Who Are We?","Why Buy Direct","Sall with boAt","2 million","AAVANTE BAR 1800v2 + BlITZ 1508 + AIRDROPS 501 ANC","AAVANTE BAR 1800v2 + AIRDROPS 501 ANC","AD 141","Awards And Appreciation","b.o.A.t. Vochers","Batman","boAt Black Friday Sale","boAt Black Friday Sale 2022","boAt FloAtverse","boAt Herables App","boAt Herables App","boAt Hub App","boAt X Baazigar","boAt x Netflix T&C","Cult Fit Gratification","Custom add to cart - Test","Customer Notification","customhomepage-NEVER-DELETE","Diwali","DWFYB","elite-club-early-access","Festival Landing page","FLASH X boAt","gift with boAt","gift with boAt","IDFC XboAt","Love Out Loud","Loyal boatheads rewards","Meet n Greet Hardik Pandiya","Meet the boAtheads","Micro Site","New Year Werables","Newsletter","quick-Checkout","Quinn API","quiz","Redirection","Rock in india","Rock with boAt","Rock with boAt TnC","Shark Tank","Shark Tank India Season 2","Smartwatches","Stream Edition","successful-login","TAta IPL 2023 with boAt","Tearms And Conditions","Test","The Fact in Your Stars | boAt Lifestyle","User Agreement","Valentine's","Welcome to boAt Days | Explore Crazy Deals,discounts on your favourite brands and more","Women's day","","Login"]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    @IBAction func SearchButtonAction(_ sender: Any) {
+        let navigaet = storyboard?.instantiateViewController(withIdentifier: "ViewControllerForsearch") as! ViewControllerForsearch
+        navigationController?.pushViewController(navigaet, animated: true)
     }
-    */
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return arrForMore.count
+    }
 
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cellLast1 = tableviewForMore.dequeueReusableCell(withIdentifier: "cellLast1") as! TableViewCellLast
+        cellLast1.labelForMore.text = "\(arrForMore[indexPath.row])"
+        return cellLast1
+    }
+    
+    @IBAction func searchButtonAction(_ sender: Any) {
+        let navigaet = storyboard?.instantiateViewController(withIdentifier: "ViewControllerForsearch") as! ViewControllerForsearch
+        navigationController?.pushViewController(navigaet, animated: true)
+    }
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        120
+    }
+    
 }
