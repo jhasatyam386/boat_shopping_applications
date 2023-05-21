959c1b07e23ee357bc7db8867cd1278cb5f05ffa
satyam jha <jhasatyam386@gamil.com>
2023-05-21 12:52:53 +0530

    21 May 2023

---
+++ boat_shopping_applications/CollectionViewCellForWatch.swift
@@ -0,0 +1,13 @@
+//
+//  CollectionViewCellForWatch.swift
+//  boat_shopping_applications
+//
+//  Created by R&W on 21/05/23.
+//
+
+import UIKit
+
+class CollectionViewCellForWatch: UICollectionViewCell {
+    
+    @IBOutlet weak var imgWatch: UIImageView!
+}
--- boat_shopping_applications/ViewControllerForHomeSmartWatchBigPosture.swift
+++ boat_shopping_applications/ViewControllerForHomeSmartWatchBigPosture.swift
@@ -7,11 +7,19 @@
 
 import UIKit
 
-class ViewControllerForHomeSmartWatchBigPosture: UIViewController {
-
+class ViewControllerForHomeSmartWatchBigPosture: UIViewController ,UICollectionViewDelegate,UICollectionViewDataSource,UICollectionViewDelegateFlowLayout{
+    
+    var arrForHomeWatch = [131,132,133,134,135,136,137,138,139,140]
+    @IBOutlet weak var shareButton: UIButton!
+    @IBOutlet weak var heartButton: UIButton!
+    
+    @IBOutlet weak var collectionviewForWatchForHomeToNextPage: UICollectionView!
     override func viewDidLoad() {
         super.viewDidLoad()
-
+//        shareButton.layer.cornerRadius = 40
+//        shareButton.layer.masksToBounds = true
+//        heartButton.layer.cornerRadius = 40
+//         heartButton.layer.masksToBounds = true
     }
     
 
@@ -28,5 +36,18 @@ class ViewControllerForHomeSmartWatchBigPosture: UIViewController {
     @IBAction func begButtonAction(_ sender: Any) {
         let navigaet = storyboard?.instantiateViewController(withIdentifier: "ViewControllerShoppingBeg") as! ViewControllerShoppingBeg
         navigationController?.pushViewController(navigaet, animated: true)
+    }
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        arrForHomeWatch.count
+    }
+
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let CellForWatch1 = collectionviewForWatchForHomeToNextPage.dequeueReusableCell(withReuseIdentifier: "cellForWatch1", for: indexPath) as! CollectionViewCellForWatch
+        CellForWatch1.imgWatch.image = UIImage(named: "\(arrForHomeWatch[indexPath.row])")
+        return CellForWatch1
+    }
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        
+        return CGSize(width: 413, height: 427)
     }
 }
