a1dbe3bfd569762c5b942242a4554049d4baf66e
kirtanBaldha <kirtan9397@gmail.com>
2023-06-13 12:42:18 +0530

    fdbfbb

---
+++ CollectionViewCell3boatWatch.swift
@@ -0,0 +1,13 @@
+//
+//  CollectionViewCell3boatWatch.swift
+//  boat_shopping_applications
+//
+//  Created by R88 on 12/06/23.
+//
+
+import UIKit
+
+class CollectionViewCell3boatWatch: UICollectionViewCell {
+    
+    @IBOutlet weak var imgForboatwatch: UIImageView!
+}
--- boat_shopping_applications/ViewControllerBigPosture2For.swift
+++ boat_shopping_applications/ViewControllerBigPosture2For.swift
@@ -7,8 +7,12 @@
 
 import UIKit
 
-class ViewControllerBigPosture2For: UIViewController {
-
+class ViewControllerBigPosture2For: UIViewController ,UICollectionViewDelegate,UICollectionViewDataSource,UICollectionViewDelegateFlowLayout{
+   
+    
+    var arrWatch = [401,402,403,404,405]
+    
+    @IBOutlet weak var collectionviewForWatch: UICollectionView!
     override func viewDidLoad() {
         super.viewDidLoad()
 
@@ -30,4 +34,18 @@ class ViewControllerBigPosture2For: UIViewController {
         let navigaet = storyboard?.instantiateViewController(withIdentifier: "ViewControllerForsearch") as! ViewControllerForsearch
         navigationController?.pushViewController(navigaet, animated: true)
     }
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return arrWatch.count
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cellForWatch = collectionviewForWatch.dequeueReusableCell(withReuseIdentifier: "cellForWatch", for: indexPath) as! CollectionViewCell3boatWatch
+        cellForWatch.imgForboatwatch.image = UIImage(named: "\(arrWatch[indexPath.row])")
+        return cellForWatch
+    }
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        
+        return CGSize(width: 413, height: 427)
+    }
+    
 }
